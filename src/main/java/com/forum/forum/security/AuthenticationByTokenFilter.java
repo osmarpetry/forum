1d96aa0a6e649ac006413084693796b341b36a61
osmarpetry <osmarpetry@gmail.com>
2020-03-18 17:17:48 -0300

    check jwt token every request (NOT USEFUL, KEEP DOING 403)

---
+++ src/main/java/com/forum/forum/security/AuthenticationByTokenFilter.java
@@ -0,0 +1,29 @@
+package com.forum.forum.security;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AuthenticationByTokenFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+        String token = recoverToken(httpServletRequest);
+
+        System.out.println(token);
+
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
+    }
+
+    private String recoverToken(HttpServletRequest request) {
+        String token = request.getHeader("Authorization");
+        if(token == null || token.isEmpty() || !token.startsWith("Bearer ")) {
+            return null;
+        }
+
+        return token.substring(7, token.length());
+    }
+}
