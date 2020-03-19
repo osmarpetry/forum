3b2b20b214465a5183f6eae43ffca23bcebf5958
osmarpetry <osmarpetry@gmail.com>
2020-03-19 15:28:49 -0300

    sweagger access to send jwt token

--- src/main/java/com/forum/forum/config/swagger/SwaggerConfigurations.java
+++ src/main/java/com/forum/forum/config/swagger/SwaggerConfigurations.java
@@ -3,11 +3,15 @@ package com.forum.forum.config.swagger;
 import com.forum.forum.model.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
 
 @EnableSwagger2
 @Configuration
@@ -20,6 +24,16 @@ public class SwaggerConfigurations {
                 .apis(RequestHandlerSelectors.basePackage("com.forum.forum"))
                 .paths(PathSelectors.ant("/**"))
                 .build()
-                .ignoredParameterTypes(User.class);
+                .ignoredParameterTypes(User.class)
+                .globalOperationParameters(
+                        Arrays.asList(
+                                new ParameterBuilder()
+                                        .name("Authorization")
+                                        .description("Header para Token JWT")
+                                        .modelRef(new ModelRef("string"))
+                                        .parameterType("header")
+                                        .required(false)
+                                        .build()));
     }
+
 }
