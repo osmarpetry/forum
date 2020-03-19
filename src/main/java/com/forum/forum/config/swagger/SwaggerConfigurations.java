47849396bb61a2e4482a2da6b67cb09195b1f804
osmarpetry <osmarpetry@gmail.com>
2020-03-19 15:22:32 -0300

    minimal setup with swagger. Without jwt auth!

---
+++ src/main/java/com/forum/forum/config/swagger/SwaggerConfigurations.java
@@ -0,0 +1,25 @@
+package com.forum.forum.config.swagger;
+
+import com.forum.forum.model.User;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfigurations {
+
+    @Bean
+    public Docket forumApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.forum.forum"))
+                .paths(PathSelectors.ant("/**"))
+                .build()
+                .ignoredParameterTypes(User.class);
+    }
+}
